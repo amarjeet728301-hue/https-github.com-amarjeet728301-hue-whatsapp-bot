dce4c029388bd1bf9ec70d89baeab12a2976b6c2
amarjeet728301-hue <amarjeet728301@gmail.com>
2026-09-16 19:17:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 const express = require('express');
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+
 app.get('/', (req,res)=>res.send('Bot is running'));
+
+const client = new Client({
+  authStrategy: new LocalAuth(),
+  puppeteer: { args: ['--no-sandbox', '--disable-setuid-sandbox'] }
+});
+
+client.on('qr', qr => {
+  console.log('QR RECEIVED');
+  qrcode.generate(qr, {small: true});
+});
+
+client.on('ready', () => console.log('WhatsApp Bot Ready!'));
+
+client.on('message', async msg => {
+  if(msg.body.toLowerCase() === 'hi') {
+    msg.reply('Hello! Bot online hai');
+  }
+});
+
+client.initialize();
+
 app.listen(PORT, ()=>console.log('Running on '+PORT));
